c58f3de46ac774d94f4072c01d27d474218074f4
Ramiz Khan <ramizkhan1199@gmail.com>
2019-05-11 22:04:44 +0530

    Removed unnecessary app.js log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side JS file is loaded')
-
 const form = document.querySelector('form')
 const search = document.querySelector('input')
 const msg1 = document.querySelector('#msg-1')
